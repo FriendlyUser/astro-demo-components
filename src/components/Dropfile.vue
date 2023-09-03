bb1e4cfba95c2b319649611e8cdcde393f97f517
David Li <davidli012345@gmail.com>
2023-09-03 10:29:45 -0700

    feat: basic file uploader in astro

---
+++ src/components/Dropfile.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="max-w-2xl mx-aut0 min-h-64 bg-white">
+    <div
+      class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      @dragover="dragover"
+      @dragleave="dragleave"
+      @drop="drop"
+    >
+      <input
+        type="file"
+        multiple
+        name="file"
+        id="fileInput"
+        class="hidden-input"
+        @change="onChange"
+        ref="file"
+        accept=".pdf,.jpg,.jpeg,.png"
+      />
+
+      <label for="fileInput" class="flex flex-col items-center justify-center pt-5 pb-6">
+
+        <div v-if="isDragging">Release to drop files here.</div>
+        <div class="flex items-center justify-center flex-col" v-else>
+            <svg class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+            <div>
+                Drop files here or <u>click here</u> to upload.
+            </div>
+        </div>
+      </label>
+      <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+
+      <div class="preview-container mt-4" v-if="files.length">
+        <div v-for="file in files" :key="file.name" class="preview-card">
+          <div>
+            <img class="preview-img" :src="generateThumbnail(file)" />
+            <p :title="file.name">
+              {{ makeName(file.name) }}
+            </p>
+          </div>
+          <div>
+            <button
+              class="ml-2"
+              type="button"
+              @click="remove(files.indexOf(file))"
+              title="Remove file"
+            >
+              <b>&times;</b>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDragging: false,
+      files: [],
+    };
+  },
+  methods: {
+    onChange() {
+      this.files = [...this.$refs.file.files];
+    },
+
+    generateThumbnail(file) {
+      let fileSrc = URL.createObjectURL(file);
+      setTimeout(() => {
+        URL.revokeObjectURL(fileSrc);
+      }, 1000);
+      return fileSrc;
+    },
+
+    makeName(name) {
+      return (
+        name.split(".")[0].substring(0, 3) +
+        "..." +
+        name.split(".")[name.split(".").length - 1]
+      );
+    },
+
+    remove(i) {
+      this.files.splice(i, 1);
+      // if files is empty, remove the dropzone
+      if (!this.files.length) {
+        this.$refs.file.value = null;
+      }
+    },
+
+    dragover(e) {
+      e.preventDefault();
+      this.isDragging = true;
+    },
+
+    dragleave() {
+      this.isDragging = false;
+    },
+
+    drop(e) {
+      e.preventDefault();
+      this.$refs.file.files = e.dataTransfer.files;
+      this.onChange();
+      this.isDragging = false;
+    },
+  },
+};
+</script>
+
+<style>
+
+.hidden-input {
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+}
+.preview-card {
+  display: flex;
+  border: 1px solid #a2a2a2;
+  padding: 5px;
+  margin-left: 5px;
+}
+.preview-img {
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid #a2a2a2;
+  background-color: #a2a2a2;
+}
+</style>
